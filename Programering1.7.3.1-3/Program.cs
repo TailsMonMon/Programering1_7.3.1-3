599f390b131a00bee942997b5a6b4ea3482cc448
Monica Björk <m.bjork95@gmail.com>
2018-04-05 08:19:35 +0200

    Add project files.

---
+++ Programering1.7.3.1-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programering1._7._3._1_3 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Är det fint väder idag? j/n: ");
+            string answer = Console.ReadLine();
+            
+            if(answer == "j") {
+                Console.WriteLine("Vi går på picknick!");
+            }
+            else if(answer == "n") {
+                Console.WriteLine("Vi stannar inne och läser en bok");
+            }
+            else {
+                Console.WriteLine("Jag förstår inte");
+            }
+            Console.ReadLine();
+        }
+    }
+}
